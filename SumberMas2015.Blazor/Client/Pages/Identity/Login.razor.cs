f8b359045a3b745ba2ede0948ee5d7690b94db18
BionStt <43228630+BionStt@users.noreply.github.com>
2022-01-18 15:00:50 +0700

    ok

---
+++ SumberMas2015.Blazor/Client/Pages/Identity/Login.razor.cs
@@ -0,0 +1,30 @@
+﻿using SumberMas2015.Blazor.Shared.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumberMas2015.Blazor.Client.Pages.Identity
+{
+    public partial class Login
+    {
+        LoginParameters loginParameters { get; set; } = new LoginParameters();
+        string error { get; set; }
+
+        async Task OnSubmit()
+        {
+            error = null;
+            try
+            {
+                await authStateProvider.Login(loginParameters);
+                navigationManager.NavigateTo("");
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+        }
+
+    }
+}
---
+++ SumberMas2015.Blazor/Client/Pages/Identity/Register.razor.cs
@@ -0,0 +1,30 @@
+﻿using SumberMas2015.Blazor.Shared.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumberMas2015.Blazor.Client.Pages.Identity
+{
+    public partial class Register
+    {
+        RegisterParameters registerParameters { get; set; } = new RegisterParameters();
+        string error { get; set; }
+
+        async Task OnSubmit()
+        {
+            error = null;
+            try
+            {
+                await authStateProvider.Register(registerParameters);
+                navigationManager.NavigateTo("");
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+        }
+
+    }
+}
